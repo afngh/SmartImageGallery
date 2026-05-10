716f6ab7caf6c5139e4ad003e88483eb2cc9794c
Shaik Afnan <shaikafnan095@gmail.com>
2026-05-10 15:59:18 +0530

    update: command-line sematic search using clip-model

--- backend/main.py
+++ backend/main.py
@@ -5,28 +5,62 @@ import torch.nn.functional as F
 import os
 
 MODEL_PATH = "./models/clip-vit-base-patch32"
-# IMAGE_FOLDER = "./data/"
+IMAGE_FOLDER = "./data/"
 
 model = CLIPModel.from_pretrained(MODEL_PATH)
-
 processor = CLIPProcessor.from_pretrained(MODEL_PATH)
 
-filename = "data/c.jpg"
+def get_image_embeddings(image_path,model,processor):
+    image_data = {}
+    image = Image.open(image_path)
+    inputs = processor(images=image, return_tensors="pt", padding=True)
+    with torch.no_grad():
+        features = model.get_image_features(pixel_values=inputs['pixel_values'])
+    features = F.normalize(features, p=2, dim=-1)
 
-image = Image.open(filename)
+    image_data['image_path'] = image_path
+    image_data['image_embeddings'] = features
 
-inputs = processor(images=image, return_tensors="pt", padding=True)
+    return image_data
 
-# print(inputs)
+def get_image_embeddings_from_folder(folder_path, model, processor):
+    image_data_list = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(('.png', '.jpg', '.jpeg')):
+            image_path = os.path.join(folder_path, filename)
+            image_data = get_image_embeddings(image_path, model, processor)
+            image_data_list.append(image_data)
+    return image_data_list
 
-# features = model.get_image_features(pixel_values=inputs['pixel_values'],return_tensors="pt")
+def get_text_embedding(text, model, processor):
+    text_data = {}
+    inputs = processor(text=[text], return_tensors="pt", padding=True)
+    features = model.get_text_features(input_ids=inputs['input_ids'],attention_mask=inputs['attention_mask'])
+    features = F.normalize(features, p=2, dim=-1)
+    text_data['text'] = text
+    text_data['text_embeddings'] = features
+    return text_data
 
-# print(features)
 
+image_embeddings = get_image_embeddings_from_folder(IMAGE_FOLDER, model, processor)
+query = input("Search : ")
+text_embedding = get_text_embedding(query.lower(), model, processor)
 
-with torch.no_grad():
-    features = model.get_image_features(pixel_values=inputs['pixel_values'])
+# Calculate similarity between text and all images
+def calculate_similarity(text_embedding, image_embeddings):
+    print(text_embedding['text_embeddings'].shape," ",image_embeddings[0]['image_embeddings'].shape)
+    similarities = []
+    for image_embedding in image_embeddings:
+        similarity = torch.matmul(text_embedding['text_embeddings'], image_embedding['image_embeddings'].T)
+        similarities.append({
+            'image_path': image_embedding['image_path'],
+            'similarity': similarity.item()
+        })
+    return similarities
 
-features = F.normalize(features.last_hidden_state, p=2, dim=-1)
+similarities = calculate_similarity(text_embedding, image_embeddings)
 
-print(features)
+# Sort by similarity
+similarities.sort(key=lambda x: x['similarity'], reverse=True)
+
+print(similarities)

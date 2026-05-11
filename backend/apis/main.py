d407d4b389454e66b0cef6e186392c80d1bf8dbf
Shaik Afnan <shaikafnan095@gmail.com>
2026-05-11 14:22:42 +0530

    feat: Integrated fontend to display sematic-search result

--- backend/apis/main.py
+++ backend/apis/main.py
@@ -1,22 +1,26 @@
-from search import load_index,get_text_embedding,get_index,model,processor
-from get_image_data import get_image_path
 from fastapi import FastAPI
-# from backend. import GalleryDB
+from fastapi.middleware.cors import CORSMiddleware
+from search import load_index, get_text_embedding, get_index, model, processor
+from get_image_data import get_image_path
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/search")
-def get_images_path(prompt : str):
+def get_images_path(prompt: str):
     query = prompt
     limit = 200
-
     index = load_index()
     text_embedding = get_text_embedding(query.lower(), model, processor)
-    indexes = get_index(text_embedding, index,limit=limit)
-    
+    indexes = get_index(text_embedding, index, limit=limit)
     image_paths = get_image_path(indexes)
-
-    return {"images":image_paths}
+    return {"images": image_paths}
 
 if __name__ == "__main__":
     import uvicorn
